19876be4bb210e9164ddbd70a2130a6ea4a58a59
Liam Watts <liam.watts271@gmail.com>
2024-11-24 12:02:41 +1100

    improve multi-threading

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,18 @@
 //!
 //! There is also `factorial`, which calculates the factorial using a single thread
 
+use std::cmp::min;
+use std::collections::HashSet;
 use std::ops::Mul;
+use std::sync::mpsc;
 use std::thread;
+
+const WINDOW_SIZE: u64 = 10_000;
+
+struct ProductResult<T> {
+    result: T,
+    start_range: u64
+}
 
 /// Calculate the factorial of a number, splitting the calculations across multiple threads
 ///
@@ -19,36 +29,45 @@ pub fn parallel_factorial<T>(n: u64, num_threads: u8) -> T
 where
     T: From<u64> + Mul<T, Output = T> + Send + 'static,
 {
-    let nums_per_thread = n / num_threads as u64; // note integer division
+    let mut result = 1.into();
+    // number to calculate the next range from
+    let mut start_range = 1;
+    // the start of each range that we have issued to other threads for calculation
+    let mut issued_ranges = HashSet::new();
 
-    // create the threads, collect them all into a vector so all the threads are spawned and running
-    let product_calculation_threads: Vec<_> = (0..num_threads)
-        .map(|thread_num| {
-            thread::spawn(move || calc_product::<T>(thread_num as u64, nums_per_thread))
-        })
-        .collect();
+    let (tx, rx) = mpsc::channel::<ProductResult<T>>();
 
-    // join the threads and accumulate the results
-    let thread_product: T = product_calculation_threads
-        .into_iter()
-        .map(|thread| thread.join().unwrap())
-        .fold(1.into(), |acc, x| acc * x);
+    while start_range <= n || issued_ranges.len() > 0 {
+        // start new threads to calculate results
+        while start_range <= n && issued_ranges.len() < num_threads as usize {
+            let tx_clone = tx.clone();
+            let next_start_range = min(n + 1, start_range + WINDOW_SIZE);
+            thread::spawn(move || {
+                let result = range_product(start_range, next_start_range - 1);
+                // discard error, parallel_factorial should keep the transmitter open until it's no longer needed, at which point failing to send is ok
+                _ = tx_clone.send(ProductResult { result, start_range})
+            });
 
-    // multiply by any number cut off at the end (because of the integer division by NUM_CORES)
-    let final_parts: T = range_product(nums_per_thread * (num_threads as u64) + 1, n);
-    thread_product * final_parts
+            issued_ranges.insert(start_range);
+            start_range = next_start_range;
+        }
+
+        // receive results
+        let window_result = rx.recv().expect("receiver should be valid for the lifetime of the function");
+        if issued_ranges.contains(&window_result.start_range) {
+            issued_ranges.remove(&window_result.start_range);
+            result = result * window_result.result;
+        }
+
+        // todo: handle failure later
+        // check if an issued_range has been in progress for too long, if so spawn a new thread to calculate it
+    }
+
+    result
 }
 
-fn calc_product<T>(offset: u64, num_to_multiply: u64) -> T
-where
-    T: From<u64> + Mul<T, Output = T>,
-{
-    let start = offset * num_to_multiply + 1; // add one to avoid multiplying by zero when offset = 0
-    let end = (offset + 1) * num_to_multiply;
-    range_product(start, end)
-}
 
-/// Sum of the numbers in [from, to] (i.e. inclusive sum)
+/// Product of the numbers in [from, to] (i.e. inclusive product)
 fn range_product<T>(from: u64, to: u64) -> T
 where
     T: From<u64> + Mul<T, Output = T>,
